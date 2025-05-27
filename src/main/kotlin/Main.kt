c7bfbaa5683dd4340a0287fe694d971b52d6874d
Lesik <Lesik_kz23@mail.ru>
2025-05-27 23:44:39 +0500

    JsopParser

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,4 @@
 package org.example
-
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
@@ -11,15 +10,5 @@ fun main() {
    val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
     val quotes: Elements = doc.select(".sc-2aegk7-2")
     println(quotes)
+
 }
-
-
-
-/*
-Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
-log(doc.title());
-Elements newsHeadlines = doc.select("#mp-itn b a");
-for (Element headline : newsHeadlines) {
-    log("%s\n\t%s",
-        headline.attr("title"), headline.absUrl("href"));
-}*/
