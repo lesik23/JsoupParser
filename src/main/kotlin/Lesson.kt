e039932cecd62c6245ffa25a183b30a64d65a920
Lesik <Lesik_kz23@mail.ru>
2025-05-27 23:56:37 +0500

    JsopParser

--- src/main/kotlin/Lesson.kt
+++ src/main/kotlin/Lesson.kt
@@ -6,7 +6,7 @@ import org.jsoup.select.Elements
 fun main() {
 
     val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
-    val quotes: Elements = doc.select(".sc-2aegk7-2")
+    val quotes: Elements = doc.select(".sc-2aegk7-2 ")
     println(quotes)
 
 }
