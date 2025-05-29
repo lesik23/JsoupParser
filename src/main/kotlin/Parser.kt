bce2115b9dca29b96e0f4036063f10e2827b9876
Lesik <Lesik_kz23@mail.ru>
2025-05-29 09:30:41 +0500

    JsopParser

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -1,12 +1,16 @@
 package org.example
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
 fun main() {
 
     val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
-    val quotes: Elements = doc.select(".sc-2aegk7-2 ")
-    println(quotes)
+    val quotes: Elements = doc.select("article")
+    for (quotesTwo: Element in quotes) {
+        println("${quotesTwo.select(".bzpNIu")}")
+    }
+            //println(quotes)
 
 }
